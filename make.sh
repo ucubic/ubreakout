5f020496a96ba3b33d981666d1ffbc027f84193f
Cubic <ucubic@proton.me>
2025-03-17 18:00:08 -0400

    refactor project structure, add WASD support

--- make.sh
+++ make.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-minify breakout.html | sed -E "
+minify src.html | sed -E "
 s/\"/'/g
+
 # inline JS into onload
 s/^<body (.*)<script>(.*)<\\/script>\$/<body onload=\"\\2\" \\1/
+
 # remove trailing tags
 s/(<\\/[a-z]+>)+\$//
-" > index.html
+" > ubreakout.html
